39582cd8fe69806d6e269b73fa8f13034849fd5e
Richard Lee <richlee81192@gmail.com>
2019-10-04 22:15:28 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,10 +4,8 @@
 # or whitespace (' ').
 class EmailAddressParser
   attr_accessor :email_addresses
-  def initialize
-    
-  end 
-  def parse
+  
+  def parse(email_addresses)
     @email_addresses.parse(", ")
   end 
   
